d944158ef81894d0db5ffe53e43815732b90e34e
Sankalp Sharma <sankalpsharma232005@gmail.com>
2025-04-01 00:40:42 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,28 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-int N,target;
-scanf("%d",&N);
-int arr[N];
-for(int i=0;i<N;i++){
-    scanf("%d",&arr[i]);
-}
-scanf("%d",target);
-int result =find(arr,N,target);
-if(result!=-1){
-    printf("%d",result);
-}
-else{
-    printf("-1");
-}
-return 0;
-}
-int find(int arr[],N,target){
-    for(int i=0;i<=N;i++){
-        if(arr[i]==target){
-            return i;
+// Function declaration (consistent with definition)
+int find(int *arr, int N, int target);
+
+// Function definition (fixed parameter types)
+int find(int arr[], int N, int target) {
+    for (int i = 0; i < N; i++) {  // Fixed: i < N (not <=)
+        if (arr[i] == target) {
+            return i;  // Return index if found
         }
-       
     }
-     return -1;
+    return -1;  // Return -1 if not found
+}
+
+int main() {
+    int N, target;
+    scanf("%d", &N);  // Read array size
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);  // Read array elements
+    }
+
+    scanf("%d", &target);  // Fixed: Added & before target
+
+    int result = find(arr, N, target);  // Call search function
+
+    if (result != -1) {
+        printf("%d\n", result);  // Fixed: Added \n
+    } else {
+        printf("-1\n");  // Fixed: Added \n
+    }
+
+    return 0;
 }
