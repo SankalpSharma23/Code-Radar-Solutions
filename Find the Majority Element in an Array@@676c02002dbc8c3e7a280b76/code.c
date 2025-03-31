06900c1555e943aed2888094caf1ce6dcc19f6cc
Sankalp Sharma <sankalpsharma232005@gmail.com>
2025-03-31 23:30:23 +0530

    Solution to Find the Majority Element in an Array

---
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    printf("Enter the size of the array: ");
+    scanf("%d", &N);
+
+    int arr[N];
+    printf("Enter %d numbers: ", N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int most_frequent = arr[0];  // Assume the first number is the most frequent
+    int max_count = 1;           // It appears at least once
+
+    for (int i = 0; i < N; i++) {
+        int count = 1;
+        for (int j = i + 1; j < N; j++) {
+            if (arr[i] == arr[j]) {
+                count++;  // Count how many times arr[i] appears
+            }
+        }
+        if (count > max_count) {
+            max_count = count;
+            most_frequent = arr[i];
+        }
+    }
+
+    printf("Most frequent number: %d (appears %d times)\n", most_frequent, max_count);
+    return 0;
+}
